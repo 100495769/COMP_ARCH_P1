ad2a49b3e63e192096bd4d92fa499bf43a4e76ce
Isabel Hernanz <isahernanz@gmail.com>
2024-11-09 22:11:24 +0100

    info soa OK + maxlevel soa (creo que OK)

--- imtool-soa/imtool-soa.cpp
+++ imtool-soa/imtool-soa.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <string>
 #include <array>
-
+#include <iostream>
 auto main(int argc, char* argv[]) -> int {    // cogemos primero los argumentos de entrada y los validamos con el validador de progargs
   std::vector<std::string> argumentos(argv, argv + argc);
 
@@ -14,19 +14,21 @@ auto main(int argc, char* argv[]) -> int {    // cogemos primero los argumentos 
     return 1; // si los parametros no son validos, terminamos
   }
 
-  std::string imagen_path = argv[0];
-  std::string salida_path = argv[1];
-  std::string operacion = argv[2];
+  std::string imagen_path = argv[1];
+  std::string salida_path = argv[2];
+  std::string operacion = argv[3];
 
   ImageSOA mi_imagen;
   mi_imagen.cargar_imagen(imagen_path);   // cargamos la imagen como tal (esta funcion ya verifica los datos)
 
   // gestionamos el tipo de operacion que sea:
   if (operacion == "info"){   // mostramos la información de los metadatos
-    mi_imagen.info(); }
+    mi_imagen.info();
+  }
 
   else if (operacion == "maxlevel") {  // la posicion 4 del vector de argumentos es la nueva intensidad. lo pasamos a entero
-    mi_imagen.maxlevel(std::stoi(argv[3])); }
+    mi_imagen.maxlevel(std::stoi(argv[4]));
+  }
   /*
   else if (operacion == "resize") {   // primero obtenemos los parametros extra: [4] es ancho, [5] es alto
     mi_imagen.resize(std::stoi(argv[4]), std::stoi(argv[5])); }
@@ -38,7 +40,10 @@ auto main(int argc, char* argv[]) -> int {    // cogemos primero los argumentos 
     std::cerr << "Error: Operacion no valida\n";
     return 1; }
 
-  mi_imagen.guardar_imagen(salida_path);  // guardamos la imagen con los cambios realizados
+  if (operacion != "info") {    // guardamos la imagen con los cambios realizados
+    mi_imagen.guardar_imagen(salida_path);
+  }
+
   return 0;
 }
 
--- libraries/common/progargs.cpp
+++ libraries/common/progargs.cpp
@@ -5,16 +5,18 @@
 #include <iostream>
 
 auto validate_parameters(int argc, std::vector<std::string> argumentos) -> bool {
-  const std::string& operacion = argumentos[2];
+  const std::string& operacion = argumentos[3];
+  std::cout << "Argc: " << argc << "\n" << argumentos[0] << "\n" << argumentos[1] << "\n" << argumentos[2] << "\n" << argumentos[3] << "\n";
+
   // int nueva_intensidad, nuevo_ancho, nuevo_alto;
   //// ICIAR METE VALORES TUYOS SI LO NECESITAS !!
 
   // gestionamos el tipo de operacion que sea:
-  if ((operacion == "info" && argc != 3) || (operacion == "maxlevel" && argc != 4) ||
-      (operacion == "resize" && argc != 5) ||
-      (operacion != "info" || "maxlevel" || "resize" || "cutfreq" || "compress")) {
+  if ((operacion == "info" && argc != 4) || (operacion == "maxlevel" && argc != 5) ||
+      (operacion == "resize" && argc != 6)) {
     // ICIAR :  4) Elimiacion de colores menos frecuentes
     // ICIAR :  5) Comprimir
+    std::cout << "Error: operacion incorrecta o numero de argumentos erroneos\n";
     return false;  // gestionamos que la operacion y el numero de argumentos son correctos
   }
   // si va bien, devolvemos true
