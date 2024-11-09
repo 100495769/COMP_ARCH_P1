f67a93ac56e61e2f567e292d18d554e46bbe526b
Isabel Hernanz <isahernanz@gmail.com>
2024-11-09 20:12:19 +0100

    para ayuda iciar

--- imtool-aos/imtool-aos.cpp
+++ imtool-aos/imtool-aos.cpp
@@ -1,3 +1,6 @@
 //
 // Created by sergio on 7/10/24.
 //
+auto main() -> int {
+  return 1;
+}
--- libraries/imgsoa/imagesoa.cpp
+++ libraries/imgsoa/imagesoa.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <cstdint>
 #include "imagesoa.hpp"
 
 ImageSOA::ImageSOA() = default;
